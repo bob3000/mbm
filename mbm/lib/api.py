5468be730f95d04f18fdaf529f71de2e9d837e6f
Robin Kautz <mail@bob3000.net>
2014-05-30 18:02:43 -0500

    api response handling

--- mbm/lib/api.py
+++ mbm/lib/api.py
@@ -1,3 +1,4 @@
+import json
 import urllib.request
 
 
@@ -13,18 +14,41 @@ class Api():
     def __call__(self, *args, **kwargs):
         post_data = kwargs["post_data"] if "post_data" in kwargs else ""
         params = {k: v for k, v in kwargs.items() if k != "post_data"}
-        url = self.url_from_method(params)
+        url = self._url_from_method(params)
         if post_data:
-            urllib.request.urlopen(url, data=post_data)
+            res = urllib.request.urlopen(url, data=post_data)
         else:
-            urllib.request.urlopen(url)
+            res = urllib.request.urlopen(url)
+        return self._handle_request(res)
 
-    def url_from_method(self, params):
+    def _url_from_method(self, params):
         url = "/".join([self.url_prefix.rstrip("/"), self.current_route])
         if params:
             params = ["=".join(map(str, e)) for e in params.items()]
             params.sort()
             params = "&".join(params)
             url = "?".join([url, params])
-
         return url
+
+    def _handle_request(self, res):
+        headers = {k.lower(): v.lower() for k, v in res.getheaders()}
+        if ("content-type" not in headers or
+                'application/json' not in headers['content-type']):
+            raise ApiException(reason="content-type is not application/json")
+        try:
+            data = json.loads(res.read())
+        except (TypeError, ValueError):
+            raise ApiException(reason="payload is not valid json")
+        return ApiResponse(data)
+
+
+class ApiResponse():
+
+    def __init__(self, data):
+        self.__dict__.update(data)
+
+
+class ApiException(Exception):
+
+    def __init__(self, reason=""):
+        self.reason = reason
--- mbm/tests/lib/test_api.py
+++ mbm/tests/lib/test_api.py
@@ -9,7 +9,15 @@ class ApiTestCase(unittest.TestCase):
 
     def setUp(self):
         self.request = urllib.request
+
+        self.http_response = MagicMock()
+        self.http_response.getheaders = \
+            lambda: [("content-type", "application/json")]
+        self.http_response.read = lambda: '{"content": "my content"}'
+
+        attrs = {'urlopen.return_value': self.http_response}
         urllib.request = MagicMock()
+        urllib.request.configure_mock(**attrs)
 
     def test_api_call(self):
         api = mbm.lib.api.Api(url_prefix="http://some.url")
@@ -17,11 +25,30 @@ class ApiTestCase(unittest.TestCase):
         urllib.request.urlopen.assert_called_with(
             "http://some.url/posts/queue?api_key=asdf98&some=value")
 
+    def test_api_call_with_payload(self):
+        api = mbm.lib.api.Api(url_prefix="http://some.url")
         api.posts(api_key="asdf98",
                   post_data={"type": "text", "title": "My new bike"})
         urllib.request.urlopen.assert_called_with(
             "http://some.url/posts?api_key=asdf98",
             data={"type": "text", "title": "My new bike"})
+
+    def test_wrong_content_type(self):
+        api = mbm.lib.api.Api(url_prefix="http://some.url")
+        self.http_response.getheaders = \
+            lambda: [("content-type", "text/html")]
+        attrs = {'urlopen.return_value': self.http_response}
+        urllib.request.configure_mock(**attrs)
+        with self.assertRaises(mbm.lib.api.ApiException):
+            api.posts_queue(api_key="asdf98", some="value")
+
+    def test_wrong_payload(self):
+        api = mbm.lib.api.Api(url_prefix="http://some.url")
+        self.http_response.read = lambda: '<html></html>'
+        attrs = {'urlopen.return_value': self.http_response}
+        urllib.request.configure_mock(**attrs)
+        with self.assertRaises(mbm.lib.api.ApiException):
+            api.posts_queue(api_key="asdf98", some="value")
 
     def tearDown(self):
         urllib.request = self.request
