e1473f7d658d788d89f50f5286d8ed9334ecdc7a
Robin Kautz <mail@bob3000.net>
2014-07-29 15:28:23 +0200

    installed pear oauth lib

--- puppet/manifests/mbm.pp
+++ puppet/manifests/mbm.pp
@@ -24,6 +24,7 @@ file { "/home/vagrant/.bash_login":
 file { "/var/www/html/oauth":
     ensure => "link",
     target => "/home/vagrant/mbm/token_procurer",
+    require => Package['php5'],
 }
 
 # basic dependencies
@@ -36,6 +37,32 @@ package { "python3-flake8": }
 # php
 package { "php5": }
 package { "php5-curl": }
+package { "php-pear": }
+
+exec { "pear upgrade":
+  command => "/usr/bin/pear upgrade && touch /root/.pear-updated",
+  require => Package['php-pear'],
+  returns => [ 0, '', ' '],
+  creates => "/root/.pear-updated",
+}
+
+exec { "pear auto_discover" :
+  command => "/usr/bin/pear config-set auto_discover 1 && touch /root/.pear-auto-discover-on",
+  require => [Package['php-pear']],
+  creates => "/root/.pear-auto-discover-on",
+}
+
+exec { "pear update-channels" :
+  command => "/usr/bin/pear update-channels && touch /root/.pear-updated-channels",
+  require => [Package['php-pear']],
+  creates => "/root/.pear-updated-channels",
+}
+
+exec {"pear install oauth":
+  command => "/usr/bin/pecl install --alldeps OAuth",
+  require => Exec['pear update-channels'],
+  creates => "/usr/share/php/docs/oauth/examples",
+}
 
 # install coverage
 exec { "easy_install3 coverage":
