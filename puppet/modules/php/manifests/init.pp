58168ba8710ca22b0d7b457b5a21b6e69b8a2c8b
Robin Kautz <mail@bob3000.net>
2014-08-04 23:50:36 +0200

    working php setup

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,38 +1,6 @@
 class php {
     package { "php5": }
-    package { "php-pear": }
-    package { "php5-dev": }
-    package { "libcurl4-openssl-dev": }
-    package { "libpcre3-dev": }
-
-    exec { "pear upgrade":
-        command => "/usr/bin/pear upgrade && touch /root/.pear-updated",
-        require => Package['php-pear'],
-        returns => [ 0, '', ' '],
-        creates => "/root/.pear-updated",
-    }
-
-    exec { "pear auto_discover" :
-        command => "/usr/bin/pear config-set auto_discover 1 && touch /root/.pear-auto-discover-on",
-        require => [Package['php-pear']],
-        creates => "/root/.pear-auto-discover-on",
-    }
-
-    exec { "pear update-channels" :
-        command => "/usr/bin/pear update-channels && touch /root/.pear-updated-channels",
-        require => [Package['php-pear']],
-        creates => "/root/.pear-updated-channels",
-    }
-
-    exec { "pecl install oauth":
-        command => "/usr/bin/pecl install --alldeps OAuth",
-        creates => "/usr/share/php/docs/oauth",
-        require => [Exec['pear update-channels'],
-                    Package['php5-dev'],
-                    Package['libcurl4-openssl-dev'],
-                    Package['libpcre3-dev'],
-                    ],
-    }
+    package { "php5-curl": }
 
     file { "/var/www/html/oauth":
         ensure => "link",
@@ -42,7 +10,17 @@ class php {
 
     file { "/etc/php5/cli/php.ini":
         source => "puppet:///modules/php/php.ini",
-        require => Exec['pecl install oauth'],
+        require => Package['php5'],
+    }
+
+    file { "/etc/php5/apache2/php.ini":
+        source => "puppet:///modules/php/php.ini",
+        require => Package['php5'],
+    }
+
+    file { "/home/vagrant/mbm/token_procurer/vendor":
+        ensure => "link",
+        target => "/home/vagrant/mbm/token_procurer/ohmy-auth/vendor",
     }
 
 }
